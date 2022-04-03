76d513021663f974d6887361a1d3e03dd63e78b8
Soufiane Irssal <soufiane.irssal@gmail.com>
2022-04-03 19:23:08 +0200

    route between signIn & infos

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import { useRouter } from 'next/router';
 
 const initialFormData = Object.freeze({
     email: "",
@@ -7,6 +8,8 @@ const initialFormData = Object.freeze({
   });
 
 function SignIn() {
+
+    const router = useRouter()
 
     const [formData, updateFormData] = React.useState(initialFormData);
 
@@ -20,7 +23,7 @@ function SignIn() {
     const handleSubmit = (e) => {
       e.preventDefault()
       console.log(formData)
-      
+      router.push('/infos')
     }
     return (
         <div className="grid min-h-screen place-items-center">
@@ -33,9 +36,11 @@ function SignIn() {
                 <input id="email" type="email" name="email" onChange={handleChange} placeholder="john.doe@company.com" autoComplete="email" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
                 <label htmlFor="password" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Password</label>
                 <input id="password" type="password" name="password" onChange={handleChange} placeholder="********" autoComplete="current-password" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+                <Link href={'/infos'}>
                 <button type="submit" onClick={handleSubmit} className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
                     Sign in
                 </button>
+                </Link>
                 <p className="flex justify-between inline-block mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">Forgot password?</p>
             </form>
           </div>
